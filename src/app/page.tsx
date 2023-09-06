2108306771046a6f75e552053800989fee0df665
Vairamuthu <mvairamuthu2003@gmail.com>
2023-09-06 11:10:16 +0530

    updated the contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,12 @@
-// Assuming "use client" is an import statement, I'll leave it as is.
 "use client";
-
-import { useState, useEffect, FC } from "react";
-import { ethers } from "ethers";
-import Counter from "./counter.json";
 import Header from "@/components/Header";
-
-interface IContract {
-  deposit: () => Promise<void>;
-  withdraw: () => Promise<void>;
-  showBalance: () => Promise<ethers.BigNumber>;
-}
-
-const Home: FC = () => {
-  const [count, setCount] = useState<number>(0);
-  const [provider, setProvider] =
-    useState<ethers.providers.Web3Provider | null>(null);
-  const [contract, setContract] = useState<IContract | null>(null);
-  const [address, setAddress] = useState<string | null>(null);
-  const [balance, setBalance] = useState<string | null>(null);
-
-  useEffect(() => {
-    async function initialize() {
-      if (typeof window.ethereum !== "undefined") {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        setProvider(provider);
-
-        const signer = provider.getSigner();
-        const address = await signer.getAddress();
-        setAddress(address);
-
-        const balance = await provider.getBalance(address);
-        setBalance(ethers.utils.formatEther(balance));
-
-        const contractAddress = "0x3A2439A5876144546D1d22b487498Af2b7e6ced1";
-        const contract = new ethers.Contract(
-          contractAddress,
-          Counter,
-          provider.getSigner()
-        ) as IContract;
-        setContract(contract);
-      }
-    }
-    initialize();
-  }, []);
-
-  useEffect(() => {
-    async function update() {
-      const updatedCount = await contract?.showBalance();
-      setCount(updatedCount?.toNumber() || 0);
-    }
-    update();
-  }, [contract]);
-
-  const handleIncrement = async () => {
-    await contract?.deposit();
-    const updatedCount = await contract?.showBalance();
-    setCount(updatedCount?.toNumber() || 0);
-  };
-
-  const handleDecrement = async () => {
-    await contract?.withdraw();
-    const updatedCount = await contract?.showBalance();
-    setCount(updatedCount?.toNumber() || 0);
-  };
-
+import { FC } from "react";
+const page: FC = () => {
   return (
-    <main>
+    <div>
       <Header />
-      <div className="py-40 text-center">
-        <h1 className="text-3xl font-bold">Counter App</h1>
-        <p className="py-3">Balance: {count}</p>
-        <p className="pt-5">Address: {address}</p>
-        <p>ETH Balance: {balance} ETH</p>
-        <button
-          className="text-white mr-2 bg-blue-600 rounded-lg mt-5 px-4 py-3"
-          onClick={handleIncrement}
-        >
-          Increment
-        </button>
-        <button
-          className="text-white bg-blue-600 rounded-lg px-4 py-3 mt-5"
-          onClick={handleDecrement}
-        >
-          Decrement
-        </button>
-      </div>
-    </main>
+    </div>
   );
 };
 
-export default Home;
+export default page;
